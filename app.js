2ea56de4a5a5140a2f4a55fcc99dc605bb0f32a0
Daniel Halabe <dan@halabe.com>
2025-05-25 12:03:26 -0700

    significant update for new data model

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
 import { RemoteStorage } from "./remoteStorage.js";
 
-let remoteStorage;
-let atKey = localStorage.getItem('atKey');
-
-if (atKey) {
-  remoteStorage = new RemoteStorage('QUIZ_RESPONSES', atKey);
-}
 
 class QuizAdmin {
   constructor() {
     this.busySpinner = document.getElementById('busy-spinner');
     this.loginButton = document.getElementById('login-btn');
+
+    const fpOptions = { enableTime: true, dateFormat: "m/d/Y, h:i K", time_24hr: false };
+    this.datePicker = flatpickr('#max-date', fpOptions);
+
+    this.searchInput = document.getElementById('student-search');
     this.listEl = document.getElementById("student-list");
+
     this.rubricFields = [
       { label: 'Declaration', key: 'declaration' },
       { label: 'Loop', key: 'loop' },
@@ -23,28 +23,33 @@ class QuizAdmin {
       { label: 'Overall', key: 'overall' }
     ];
 
-    this.loginButton.addEventListener('click', async () => {
-      atKey = await UIkit.modal.prompt('Enter the admin key');
-      if (atKey) {
-        remoteStorage = new RemoteStorage('QUIZ_RESPONSES', atKey);
-        localStorage.setItem('atKey', atKey);
-        this.start();
-      }
-    });
+    this.loginButton.addEventListener('click', async () => this.onClickedLogin());
+    this.datePicker.set("onChange", [(selectedDates) => this.onFilterChange(selectedDates)]);
+    this.searchInput.addEventListener('input', e => this.onFilterChange(e));
 
-    this.searchInput = document.getElementById('student-search');
-    this.searchInput.addEventListener('input', (e) => {
-      this.filteredRecords = this.filteredSubmissions();
-      this.renderStudentList();
-      this.selectStudent(null, null);
-    });
+    this.remoteStorage = null;
+    this.atKey = localStorage.getItem('atKey');
+    if (this.atKey) {
+      this.remoteStorage = new RemoteStorage('QUIZ_RESPONSES', this.atKey);
+    }
   }
 
+  /*************************************************************************************/
+  // Get data and start
+
+  /*
+      Data is student responses keyed by identity and time, possibly >1 responses per identity
+      {
+        "mi-9444@2025-05-24T20:58:07.102Z" --> { {firstName:"Bart", ... scores: { declaration: "2"} },
+        ...
+      }
+  */
+
   async start() {
-    if (remoteStorage) {
+    if (this.remoteStorage) {
       this.loginButton.hidden = true;
       this.submissions = await this.fetchSubmissions();
-      this.filteredRecords = this.filteredSubmissions();
+      this.filteredSubmissions = this.filterSubmissions();
 
       this.editor = await this.startEditor();
       this.render();
@@ -54,51 +59,108 @@ class QuizAdmin {
     }
   }
 
-  // Returns [ { hashedID, submissions: [ { creationDate, firstName, lastName, ... } ] } ]
-  // The submissions arrays will be ordered in time, newest first
   async fetchSubmissions() {
-    const submissionIndex = {};
+    const submissions = [];
 
-    const keys = await remoteStorage.keys();
+    const keys = await this.remoteStorage.keys();
     for (let key of keys) {
-      const [hashedID, _] = key.split('@');
-      const { value, metadata } = await remoteStorage.getItemWithMetadata(key);
-      value.creationTime = new Date(metadata.creationTime);
-      submissionIndex[hashedID] = [...(submissionIndex[hashedID] ?? []), value];
+      const { value, metadata } = await this.remoteStorage.getItemWithMetadata(key);
+      const studentID = value.studentID;
+      const creationTime = new Date(metadata.creationTime);
+      const fullName = `${value.lastName}, ${value.firstName}`;
+      const matchString = `${fullName} ${studentID}`.toLowerCase();
+      submissions.push({ key, studentID, creationTime, fullName, matchString, value });
     }
 
-    // Sort the keys by students' full names
-    const hashedIDs = Object.keys(submissionIndex).sort((a, b) => {
-      const nameA = `${a.lastName},${a.firstName}`.toLowerCase();
-      const nameB = `${b.lastName},${b.firstName}`.toLowerCase();
-      return nameA.localeCompare(nameB);
+    submissions.sort((a, b) => {
+      const nameCompare = a.fullName.localeCompare(b.fullName);
+      return nameCompare == 0 ? a.creationTime - b.creationTime : nameCompare;
     });
 
-    const submissions = [];
-    for (const hashedID of hashedIDs) {
-      const sortedSubmissions = submissionIndex[hashedID].sort((a, b) => b.creationTime - a.creationTime);
-      const record = { hashedID, submissions: sortedSubmissions };
-      submissions.push(record);
-    }
     return submissions;
   }
 
-  filteredSubmissions() {
-    const matchString = this.searchInput.value.toLowerCase();
-    const maxDate = this.datePicker?.selectedDates?.[0];
+  filterSubmissions() {
+    const searchString = this.searchInput.value.toLowerCase();
+    const maxDate = this.datePicker?.selectedDates?.[0] ?? new Date('9999-12-31T23:59:59.999Z');
+    const idIndex = new Map();
 
-    const matches = [];
-    for (let record of this.submissions) {
-      for (let submission of record.submissions) {
-        const matchWith = `${submission.lastName}, ${submission.firstName} ${submission.studentID}`;
-        const alphaMatch = matchWith.toLowerCase().includes(matchString);
-        const timeMatch = maxDate ? submission.creationTime < maxDate : true;
-        if (alphaMatch && timeMatch) {
-          matches.push(submission);
-        }
+    for (const submission of this.submissions) {
+      if (submission.creationTime > maxDate) continue;
+      if (searchString && !submission.matchString.includes(searchString)) continue;
+
+      const priorSubmission = idIndex.get(submission.studentID);
+      if (!priorSubmission || submission.creationTime > priorSubmission.creationTime) {
+        idIndex.set(submission.studentID, submission);
       }
     }
-    return matches;
+    return Array.from(idIndex.values());
+  }
+
+  /*************************************************************************************/
+  // Event handling
+
+  async onClickedLogin() {
+    this.atKey = await UIkit.modal.prompt('Enter the admin key');
+    if (this.atKey) {
+      this.remoteStorage = new RemoteStorage('QUIZ_RESPONSES', this.atKey);
+      localStorage.setItem('atKey', this.atKey);
+      this.start();
+    }
+  }
+
+  onFilterChange() {
+    this.filteredSubmissions = this.filterSubmissions();
+    this.renderStudentList();
+    this.onSelectSubmission(null, null);
+  }
+
+  onSelectSubmission(li, index) {
+    this.listEl = document.getElementById("student-list");
+    this.listEl.querySelectorAll("li").forEach(el => el.classList.remove("uk-active"));
+
+    if (!li) {
+      this.clearSelection();
+    } else {
+      li.classList.add("uk-active");
+      this.selectedSubmission = this.filteredSubmissions[index];
+      this.selectedLi = li;
+      this.renderSubmission(this.selectedSubmission);
+      this.scoringSelects.forEach(selectEl => selectEl.disabled = false);
+    }
+  }
+
+  async onScoreSelectorChange(selectEl) {
+    if (!this.selectedSubmission) return;
+
+    const id = selectEl.id;
+    const value = selectEl.value;
+    this.setSelectValue(selectEl, value);
+    (this.selectedSubmission.value.scores ||= {})[selectEl.id] = value;
+
+    // when overall is set, set any uninitialized selectors to the same value
+    if (id == 'overall') {
+      this.selectedLi.textContent = this.formatedName(this.selectedSubmission);
+
+      this.scoringSelects.forEach(selectEl => {
+        if (selectEl.id !== id && selectEl.value == '') {
+          this.setSelectValue(selectEl, value);
+          (this.selectedSubmission.value.scores ||= {})[selectEl.id] = value;
+        }
+      });
+    }
+
+    // save
+    this.busySpinner.hidden = false;
+    await this.remoteStorage.setItem(this.selectedSubmission.key, this.selectedSubmission.value);
+    this.busySpinner.hidden = true;
+  }
+
+  /*************************************************************************************/
+  // Render
+
+  static truncate(str, length = 18) {
+    return str.length > length ? str.slice(0, length - 1) + '…' : str;
   }
 
   async render() {
@@ -106,7 +168,6 @@ class QuizAdmin {
       const iso = selectedDates[0]?.toISOString();
     }
     this.loginButton.hidden = true;
-    this.datePicker = flatpickr("#max-date", { enableTime: true, onChange: onDateChange });
     this.renderStudentList();
     // rubric selectors
     let rubricHTML = '';
@@ -122,7 +183,7 @@ class QuizAdmin {
     this.scoringSelects = [...document.querySelectorAll('.rubric-select')];
     this.scoringSelects.forEach(selectEl => {
       selectEl.addEventListener('change', (event) => {
-        this.scoringSelectChanged(selectEl);
+        this.onScoreSelectorChange(selectEl);
       });
     });
   }
@@ -147,14 +208,65 @@ class QuizAdmin {
 
   renderStudentList() {
     this.listEl.innerHTML = "";
-    this.filteredRecords.forEach((record, index) => {
+    this.filteredSubmissions.forEach((submission, index) => {
       const li = document.createElement("li");
-      this.setStudentListName(li, record);
+      li.textContent = this.formatedName(submission);
       li.style.cursor = 'pointer';
-      li.addEventListener("click", () => this.selectStudent(li, index));
+      li.addEventListener("click", () => this.onSelectSubmission(li, index));
       this.listEl.appendChild(li);
     });
   }
+
+  formatedName(submission) {
+    const check = submission.value.scores?.overall ? '  \u2713' : '';
+    return `${QuizAdmin.truncate(submission.fullName, 28)}${check}`;
+  }
+
+  formatedDate(date) {
+    const options = { month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+    return date.toLocaleString(undefined, options);
+  }
+
+  renderSubmission(submission) {
+    const { firstName, lastName, studentID, hashedID, response, scores } = submission.value;
+    document.getElementById("student-firstName").textContent = QuizAdmin.truncate(firstName);
+    document.getElementById("student-lastName").textContent = QuizAdmin.truncate(lastName);
+    document.getElementById("student-id").textContent = studentID;
+    document.getElementById("hashed-id").textContent = hashedID;
+    document.getElementById("creation-time").textContent = this.formatedDate(submission.creationTime);
+
+    this.rubricFields.forEach(rubricField => {
+      const rubricKey = rubricField.key;
+      const selectEl = this.scoringSelects.find(selectEl => selectEl.id === rubricKey);
+      const value = (scores && scores[rubricKey]) || '';
+      this.setSelectValue(selectEl, value);
+    });
+    this.editor.setValue(response);
+  }
+
+  setSelectValue(selectEl, value) {
+    const colorForValue = value => ({ '1': '#ffdddd', '2': '#ffeb99', '3': '#b3ff99' }[value] || '#ffffff');
+    selectEl.value = value;
+    selectEl.style.backgroundColor = colorForValue(value);
+  }
+
+  clearSelection() {
+    this.selectedSubmission = null;
+    this.selectedLi = null;
+
+    ['student-firstName', 'student-lastName', 'student-id', 'hashed-id'].forEach(id => {
+      document.getElementById(id).textContent = '';
+    });
+
+    // Clear rubric fields
+    this.rubricFields.forEach(rubricField => {
+      const selectEl = this.scoringSelects.find(selectEl => selectEl.id === rubricField.key);
+      this.setSelectValue(selectEl, '');
+      selectEl.disabled = true;
+    });
+    this.editor.setValue('');
+  }
+
 
   async startEditor() {
     require.config({ paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs' } });
@@ -173,128 +285,9 @@ class QuizAdmin {
     return editor;
   }
 
-  // update the selector and change the selected record's score
-  setSelectValue(selectEl, value) {
-    const colorForValue = value => ({ '1': '#ffdddd', '2': '#ffeb99', '3': '#b3ff99' }[value] || '#ffffff');
-    selectEl.value = value;
-    selectEl.style.backgroundColor = colorForValue(value);
-  }
-
-  truncate(str, length = 18) {
-    return str.length > length ? str.slice(0, length - 1) + '…' : str;
-  };
-
-  setStudentListName(listEl, record) {
-    const check = record.scores?.overall ? '\u2713' : '';
-    const fullName = `${record.lastName}, ${record.firstName}`;
-    listEl.textContent = `${this.truncate(fullName, 28)}, ${check}`;
-  }
-
-  async scoringSelectChanged(selectEl) {
-    if (!this.selectedRecord) return;
-
-    const id = selectEl.id;
-    const value = selectEl.value;
-    this.setSelectValue(selectEl, value);
-    (this.selectedRecord.scores ||= {})[selectEl.id] = value;
-
-    // when overall is set, set any uninitialized selectors to the same value
-    if (id == 'overall') {
-      this.setStudentListName(this.selectedLi, this.selectedRecord);
-
-      this.scoringSelects.forEach(selectEl => {
-        if (selectEl.id !== id && selectEl.value == '') {
-          this.setSelectValue(selectEl, value);
-          (this.selectedRecord.scores ||= {})[selectEl.id] = value;
-        }
-      });
-    }
-
-    // save the selected record
-    this.busySpinner.hidden = false;
-    await remoteStorage.setItem(this.selectedRecord.hashedID, this.selectedRecord);
-    this.busySpinner.hidden = true;
-  }
-
-  selectStudent(li, index) {
-    this.listEl = document.getElementById("student-list");
-    this.listEl.querySelectorAll("li").forEach(el => el.classList.remove("uk-active"));
-
-    if (!li) {
-      this.clearSelection();
-    } else {
-      li.classList.add("uk-active");
-      this.selectedRecord = this.filteredRecords[index];
-      this.selectedLi = li;
-      this.renderRecord(this.selectedRecord);
-      this.scoringSelects.forEach(selectEl => selectEl.disabled = false);
-    }
-  }
-
-  clearSelection() {
-    this.selectedRecord = null;
-    this.selectedLi = null;
-
-    ['student-firstName', 'student-lastName', 'student-id', 'hashed-id'].forEach(id => {
-      document.getElementById(id).textContent = '';
-    });
-
-    // Clear rubric fields
-    this.rubricFields.forEach(rubricField => {
-      const selectEl = this.scoringSelects.find(selectEl => selectEl.id === rubricField.key);
-      this.setSelectValue(selectEl, '');
-      selectEl.disabled = true;
-    });
-    this.editor.setValue('');
-  }
-
-  renderRecord(record) {
-    const { firstName, lastName, studentID, hashedID, response } = record;
-    document.getElementById("student-firstName").textContent = this.truncate(firstName);
-    document.getElementById("student-lastName").textContent = this.truncate(lastName);
-    document.getElementById("student-id").textContent = studentID;
-    document.getElementById("hashed-id").textContent = hashedID;
-
-    this.rubricFields.forEach(rubricField => {
-      const rubricKey = rubricField.key;
-      const selectEl = this.scoringSelects.find(selectEl => selectEl.id === rubricKey);
-      const value = (record.scores && record.scores[rubricKey]) || '';
-      this.setSelectValue(selectEl, value);
-    });
-
-    this.editor.setValue(response);
-  }
 }
 /*************************************************************************************************/
 /*************************************************************************************************/
 
 const quiz = new QuizAdmin();
 quiz.start();
-
-
-
-/**
- * 
-    // for date filtering
-    // Select the most recent submission for each (but not more recent than max creation date)
-
-    for (const key of hashedIDs) {
-      const array = submissions[key];
-      if (!this.maxCreationDate) {
-        responses.push(array[0]); // just take the (newest)
-      } else {
-        const newestBeforeMax = array.find(obj => obj.creationTime <= this.maxCreationDate);
-        if (newestBeforeMax) responses.push(newestBeforeMax);
-      }
-    }
-
-
-    // for alpha filtering
-
-      const query = e.target.value.toLowerCase();
-      const recordMatches = record => {
-        const matchWith = (`${record.lastName}, ${record.firstName} ${record.studentID}`)
-        return matchWith.toLowerCase().includes(query);
-      };
-
- */
